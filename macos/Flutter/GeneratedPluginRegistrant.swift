94aa5493586bbddb8f45bc82735e6241696a24d9
valdydev7 <valdydev7@gmail.com>
2026-03-05 23:59:46 +0000

    # modified:   ios/Podfile.lock 	# modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	# modified:   pubspec.lock 	# modified:   pubspec.yaml

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import geolocator_apple
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
